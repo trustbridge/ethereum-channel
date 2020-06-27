d9bd6da7b390c850d44e8127d85c607904d28ca8
roleyfoley <roleyfoley@gmail.com>
2020-06-27 20:04:45 +1000

    feat(worker): allow providing config file as an env var

--- inbound-event-listener/worker/docker/entrypoint.sh
+++ inbound-event-listener/worker/docker/entrypoint.sh
@@ -3,4 +3,11 @@ set -e
 
 echo "Container started"
 
+# Allow for translating a env var into a local config file
+if [[ -n "${CONFIG_FILE_VALUE}" ]]; then
+    echo "Config file provided by env var. Writing to file..."
+    echo "${CONFIG_FILE_VALUE}" > /tmp/config.json
+    export CONFIG_FILE="/tmp/config.json"
+fi
+
 exec "$@"
