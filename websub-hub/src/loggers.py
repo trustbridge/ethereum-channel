480c633045ae9acdd54e8e9c2391aabc43e50148
Neketek <neketek@gmail.com>
2020-10-21 17:53:17 +0300

    WIP: removed old websub-hub subproject

--- websub-hub/src/loggers.py
+++
@@ -1,8 +0,0 @@
-from src import conf
-from libtrustbridge.utils.loggers import logging
-
-logging.getLogger("requests").setLevel(logging.WARNING)
-logging.getLogger("urllib3").setLevel(logging.WARNING)
-
-
-logger = logging.getLogger(conf.SERVICE_NAME)
--- websub-hub/src/processors/__init__.py
+++
@@ -1,8 +0,0 @@
-import time
-from libtrustbridge.websub.processors import Processor
-
-
-class SelfIteratingProcessor(Processor):
-    def start(self):  # pragma: no cover
-        for iteration in self:
-            time.sleep(1)
