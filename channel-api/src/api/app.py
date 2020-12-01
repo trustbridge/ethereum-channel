b1cd7b9c678ab5262e9fde839a0b8be8b57adda2
Neketek <neketek@gmail.com>
2020-12-01 17:07:20 +0200

    DONE: gas pricing strategies

--- channel-api/src/api/app.py
+++ channel-api/src/api/app.py
@@ -2,6 +2,7 @@ import urllib
 from box import Box
 from flask import Flask
 from web3 import Web3
+from web3.gas_strategies import time_based as gas_price_strategies
 from libtrustbridge.errors import handlers as error_handlers
 from src import repos
 from . import views as api
@@ -15,6 +16,18 @@ def create_app(config: Box = None):
     with app.app_context():
 
         app.web3 = Web3(Web3.HTTPProvider(config.HTTP_BLOCKCHAIN_ENDPOINT))
+
+        if config.BLOCKCHAIN_GAS_PRICE_STRATEGY == 'fast':
+            app.web3.eth.setGasPriceStrategy(gas_price_strategies.fast_gas_price_strategy)
+        elif config.BLOCKCHAIN_GAS_PRICE_STRATEGY == 'medium':
+            app.web3.eth.setGasPriceStrategy(gas_price_strategies.medium_gas_price_strategy)
+        else:
+            raise ValueError(
+                'BLOCKCHAIN_GAS_PRICE must be in ["fast", "medium"], got: "{}"'.format(
+                    config.BLOCKCHAIN_GAS_PRICE_STRATEGY
+                )
+            )
+
         app.repos = Box(
             subscriptions=repos.Subscriptions(config=config.SUBSCRIPTIONS_REPO),
             contract=repos.Contract(config=config.CONTRACT_REPO)
